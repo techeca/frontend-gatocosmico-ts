8c6758488538f1a3de0e862f7105ca6d5d7593e3
techeca <jp.vasquez@outlook.com>
2024-06-25 03:05:58 -0400

    Fix useLoading hook to use TypeScript types in useLoading.tsx

--- src/hooks/useLoading.tsx
+++ src/hooks/useLoading.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
  
 //para FOUC
-export default function useLoading(isLoading, setIsLoading) {
+export default function useLoading(isLoading: boolean, setIsLoading: (value: boolean) => void) {
   useEffect(() => {
     if (isLoading === false) {
       setIsLoading(true);
